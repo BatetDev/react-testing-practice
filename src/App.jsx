76c509cc4c71a34831c6cfc7db2ab49203a6f8a1
fr4nbtt <batet.francisco@gmail.com>
2026-09-15 17:50:25 -0300

    test: assert all list items render

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <ul className='animals'>
+        <li>Cat</li>
+        <li>Whale</li>
+        <li>Lion</li>
+        <li>Elephant</li>
+        <li>Rhino</li>
+      </ul>
     </div>
   );
 }
--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import { expect } from 'vitest';
 
 test('render h1 element', () => {
   render(<App />);
+  screen.debug();
   expect(screen.getByText('Hello World')).toBeInTheDocument();
+});
+
+test('list contains 5 animals', () => {
+  render(<App />);
+
+  const listElement = screen.getByRole('list');
+  const listItems = screen.getAllByRole('listitem');
+
+  expect(listElement).toBeInTheDocument();
+  expect(listElement).toHaveClass('animals');
+  expect(listItems.length).toEqual(5);
 });
